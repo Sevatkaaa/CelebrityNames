d5fc976c7527d6d69938566d50d697e3a2fab726
Sevatkaaa <sevatkaaa@gmail.com>
2022-10-07 20:17:24 +0300

    Set up project with db

---
+++ src/main/java/com/sov/CelebrityNames.java
@@ -0,0 +1,11 @@
+package com.sov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelebrityNames {
+    public static void main(String[] args) {
+        SpringApplication.run(CelebrityNames.class, args);
+    }
+}
